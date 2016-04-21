02720ebbed19eb4d1eef6e3315feb0273987fff1
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-04-21 15:42:37 +0600

    Update class_file.py

--- class_file.py
+++ class_file.py
@@ -8,19 +8,20 @@ class Movie():
     ... this is the class documentation docstring ...
     insert here some information about your class
     """
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-	""" ... this is the constructor method docstring ...
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ ... this is the constructor method docstring ...
         here you should include some information about the function behavior and
         its input and outputs (if applicable)
         """
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-	# function for showing trailer
-	def show_trailer(self):
-	""" show_trailer() docstring...
+    # function for showing trailer
+    def show_trailer(self):
+        """ show_trailer() docstring...
         here you should include information about the function
         """
-		webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
